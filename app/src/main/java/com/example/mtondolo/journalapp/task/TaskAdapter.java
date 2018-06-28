41a336964c098d0953f9a103883926d32a7b146e
mtondolo <mtondolo@gmail.com>
2018-06-28 16:40:43 +0200

    feat: Create viewholder and adapter

---
+++ app/src/main/java/com/example/mtondolo/journalapp/task/TaskAdapter.java
@@ -0,0 +1,111 @@
+package com.example.mtondolo.journalapp.task;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.mtondolo.journalapp.R;
+import com.example.mtondolo.journalapp.data.TaskEntity;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+
+    // Constant for date format
+    private static final String DATE_FORMAT = "dd/MM/yyy";
+
+    // Member variable to handle item clicks
+    final private ItemClickListener mItemClickListener;
+
+    // Class variables for the List that holds task data and the Context
+    private List<TaskEntity> mTaskEntities;
+    private Context mContext;
+
+    // Date formatter
+    private SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
+
+    /**
+     * Constructor for the TaskAdapter that initializes the Context.
+     *
+     * @param context  the current Context
+     * @param listener the ItemClickListener
+     */
+    public TaskAdapter(Context context, ItemClickListener listener) {
+        mContext = context;
+        mItemClickListener = listener;
+    }
+
+    // Inner class for creating ViewHolders
+    class TaskViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        // Class variables for the task description and priority TextViews
+        TextView taskDescriptionTextView;
+        TextView enteredAtTextView;
+
+        /**
+         * Constructor for the TaskViewHolders.
+         *
+         * @param itemView The view inflated in onCreateViewHolder
+         */
+        public TaskViewHolder(View itemView) {
+            super(itemView);
+
+            taskDescriptionTextView = itemView.findViewById(R.id.tv_taskDescription);
+            enteredAtTextView = itemView.findViewById(R.id.tv_taskDate);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int elementId = mTaskEntities.get(getAdapterPosition()).getId();
+            mItemClickListener.onItemClickListener(elementId);
+        }
+    }
+
+    @Override
+    public TaskViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Inflate the task_item to a view
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.task_item, parent, false);
+
+        return new TaskViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(TaskViewHolder holder, int position) {
+        // Determine the values of the wanted data
+        TaskEntity taskEntity = mTaskEntities.get(position);
+        String description = taskEntity.getDescription();
+        String enteredAt = dateFormat.format(taskEntity.getEnteredAt());
+
+        //Set values
+        holder.taskDescriptionTextView.setText(description);
+        holder.enteredAtTextView.setText(enteredAt);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mTaskEntities == null) {
+            return 0;
+        }
+        return mTaskEntities.size();
+    }
+
+    /**
+     * When data changes, this method updates the list of taskEntries
+     * and notifies the adapter to use the new values on it
+     */
+    public void setTasks(List<TaskEntity> taskEntities) {
+        mTaskEntities = taskEntities;
+        notifyDataSetChanged();
+    }
+
+    public interface ItemClickListener {
+        void onItemClickListener(int itemId);
+    }
+}
