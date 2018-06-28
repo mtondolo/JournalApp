f8540cad06aad586f0881d2c5602b86bda9217a8
mtondolo <mtondolo@gmail.com>
2018-06-28 19:50:32 +0200

    feat: Wire up recyclerview with adapter and linearlayoutmanager

--- app/src/main/java/com/example/mtondolo/journalapp/task/TaskActivity.java
+++ app/src/main/java/com/example/mtondolo/journalapp/task/TaskActivity.java
@@ -2,19 +2,45 @@ package com.example.mtondolo.journalapp.task;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.mtondolo.journalapp.R;
 import com.example.mtondolo.journalapp.data.TaskDatabase;
 
-public class TaskActivity extends AppCompatActivity {
+import static android.widget.GridLayout.VERTICAL;
+
+public class TaskActivity extends AppCompatActivity implements TaskAdapter.ItemClickListener{
+    // Constant for logging
+    private static final String TAG = TaskActivity.class.getSimpleName();
+    // Member variables for the adapter and RecyclerView
+    private RecyclerView mRecyclerView;
+    private TaskAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_act);
-        
-        TaskDatabase mDb = TaskDatabase.getInstance(getApplicationContext());
-        mDb.taskDao();
 
+        // Set the RecyclerView to its corresponding view
+        mRecyclerView = findViewById(R.id.recyclerViewTasks);
+
+        // Set the layout for the RecyclerView to be a linear layout, which measures and
+        // positions items within a RecyclerView into a linear list
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the adapter and attach it to the RecyclerView
+        mAdapter = new TaskAdapter(this, this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), VERTICAL);
+        mRecyclerView.addItemDecoration(decoration);
+
+    }
+
+    @Override
+    public void onItemClickListener(int itemId) {
+        // Launch AddTaskActivity adding the itemId as an extra in the intent
     }
 }
