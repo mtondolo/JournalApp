38ddcc23a2ee426143f076de42805ebfa1d1579f
mtondolo <mtondolo@gmail.com>
2018-06-29 15:21:01 +0200

    feat: Save tasks into database

--- app/src/main/java/com/example/mtondolo/journalapp/task/AddTaskActivity.java
+++ app/src/main/java/com/example/mtondolo/journalapp/task/AddTaskActivity.java
@@ -8,8 +8,20 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 
 import com.example.mtondolo.journalapp.R;
+import com.example.mtondolo.journalapp.data.TaskDatabase;
+import com.example.mtondolo.journalapp.data.TaskEntity;
+
+import java.util.Date;
 
 public class AddTaskActivity extends AppCompatActivity {
+
+    // Extra for the task ID to be received in the intent
+    public static final String EXTRA_TASK_ID = "extraTaskId";
+    // Extra for the task ID to be received after rotation
+    public static final String INSTANCE_TASK_ID = "instanceTaskId";
+
+    // Constant for default task id to be used when not in update mode
+    private static final int DEFAULT_TASK_ID = -1;
 
     // Constant for logging
     private static final String TAG = AddTaskActivity.class.getSimpleName();
@@ -18,12 +30,30 @@ public class AddTaskActivity extends AppCompatActivity {
     EditText mDescriptionEditText, mDetailsEditText;
     Button mButton;
 
+    // Member variable for the Database
+    private TaskDatabase mDb;
+
+    private int mTaskId = DEFAULT_TASK_ID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addtask_act);
 
         initViews();
+
+        //Initialize member variable for the data base
+        mDb = TaskDatabase.getInstance(getApplicationContext());
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(INSTANCE_TASK_ID)) {
+            mTaskId = savedInstanceState.getInt(INSTANCE_TASK_ID, DEFAULT_TASK_ID);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(INSTANCE_TASK_ID, mTaskId);
+        super.onSaveInstanceState(outState);
     }
 
     /**
@@ -34,5 +64,26 @@ public class AddTaskActivity extends AppCompatActivity {
         mDetailsEditText = findViewById(R.id.editTextTaskDetails);
 
         mButton = findViewById(R.id.saveButton);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSaveButtonClicked();
+            }
+        });
     }
+
+    /**
+     * onSaveButtonClicked is called when the "save" button is clicked.
+     * It retrieves user input and inserts that new task data into the underlying database.
+     */
+    public void onSaveButtonClicked() {
+        String taskDescription = mDescriptionEditText.getText().toString();
+        String taskDetails = mDetailsEditText.getText().toString();
+        Date date = new Date();
+        TaskEntity taskEntity = new TaskEntity(taskDescription, taskDetails, date);
+        mDb.taskDao().insertTask(taskEntity);
+        finish();
+    }
+
+
 }
